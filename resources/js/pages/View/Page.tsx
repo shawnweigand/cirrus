04aed82f82d9c25f28ce55daf8204e73effb181a
Shawn Weigand <shawnweigand1@gmail.com>
2025-06-12 14:06:21 -0400

    feat: :sparkles: add cards

--- resources/js/pages/View/Page.tsx
+++ resources/js/pages/View/Page.tsx
@@ -8,6 +8,8 @@ import { z } from "zod";
 import { useForm as useReactForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod";
 import Form from "./Form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 type ExtendedPageProps = { }
 
@@ -35,11 +37,33 @@ export default function Page({ }: ExtendedPageProps) {
         return () => window.removeEventListener('message', handleMessage);
     }, []);
 
+
     return (
-        <>
-            {schemas.map((schema, index) => (
-                <Form key={index} schema={schema.content} />
-            ))}
-        </>
-      )
+        <div className="w-full flex justify-center">
+            <div className="flex w-full max-w-4xl flex-col gap-6">
+            {schemas.length > 0 &&
+                <Tabs defaultValue={schemas[0].title} className="w-full pt-16">
+                    <TabsList>
+                        {schemas.map((schema, index) => (
+                            <TabsTrigger value={schema.title} key={index}>{schema.title}</TabsTrigger>
+                        ))}
+                    </TabsList>
+                    {schemas.map((schema, index) => (
+                        <TabsContent value={schema.title} key={index}>
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle>{schema.title}</CardTitle>
+                                    <CardDescription>This is a description of the module</CardDescription>
+                                </CardHeader>
+                                <CardContent className="grid gap-6">
+                                    <Form key={index} schema={schema.content} />
+                                </CardContent>
+                            </Card>
+                        </TabsContent>
+                    ))}
+                </Tabs>
+            }
+            </div>
+        </div>
+    )
 }
