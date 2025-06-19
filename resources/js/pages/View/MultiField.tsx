ddf4783a37ca0d105685384674903c1fb98f32b2
Shawn Weigand <shawnweigand1@gmail.com>
2025-06-19 08:53:21 -0400

    cleanup

--- resources/js/pages/View/MultiField.tsx
+++ resources/js/pages/View/MultiField.tsx
@@ -1,16 +1,6 @@
-import React, { useState } from "react";
-import { Cat, Dog, Fish, Rabbit, Turtle } from "lucide-react";
 import { MultiSelect } from "@/components/ui/multi-select";
 import { UseFormReturn } from "react-hook-form";
 import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-
-const frameworksList = [
-  { value: "react", label: "React", icon: Turtle },
-  { value: "angular", label: "Angular", icon: Cat },
-  { value: "vue", label: "Vue", icon: Dog },
-  { value: "svelte", label: "Svelte", icon: Rabbit },
-  { value: "ember", label: "Ember", icon: Fish },
-];
 
 type ExtendedPageProps = {
     templateField: App.Data.Form.MultiFieldData
@@ -18,7 +8,6 @@ type ExtendedPageProps = {
 }
 
 export default function MultiField({ templateField, form }: ExtendedPageProps) {
-  const [selectedFrameworks, setSelectedFrameworks] = useState<string[]>(["react", "angular"]);
 
   return (
     <FormField
